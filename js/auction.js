16be9237b7da91e438b86a320c85f19c3c6c49e7
tom981105-web <tom981105@gmail.com>
2026-06-09 20:04:59 +0900

    Delete js directory

--- js/auction.js
+++
@@ -1,4 +0,0 @@
-// auction.js - 거래소 모듈 분리 준비 파일
-// 현재 4~7단계에서는 기존 fishing.html 내부 거래소 로직을 보존합니다.
-// 다음 단계에서 renderMultiAuction/createAuctionItem/buyAuctionItem/editAuctionItem/cancelAuctionItem을 이 파일로 이동하면 됩니다.
-window.FishingAuctionModule = window.FishingAuctionModule || { version: 'split-ready' };
