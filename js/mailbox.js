b06a73037355411806388c2abe46ebde68490358
tom981105-web <tom981105@gmail.com>
2026-06-09 20:05:16 +0900

    Add files via upload

---
+++ js/mailbox.js
@@ -0,0 +1,4 @@
+// mailbox.js - 우편함 모듈 분리 준비 파일
+// 현재 4~7단계에서는 기존 fishing.html 내부 우편함 로직을 보존합니다.
+// 다음 단계에서 addBossMail/importBossMailsFromFirebase/claimBossMail/openBossMailboxPopup을 이 파일로 이동하면 됩니다.
+window.FishingMailboxModule = window.FishingMailboxModule || { version: 'split-ready' };
